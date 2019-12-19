1662a98625ebc8a0ce1778708ca348ef9aece115
Chris Basham <chrisdbasham0317@gmail.com>
2019-12-19 13:51:46 -0700

    Fix settings for heroku deployment

--- intreped/settings.py
+++ intreped/settings.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = ['https://intreped.herokuapp.com/']
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
+    # 'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
