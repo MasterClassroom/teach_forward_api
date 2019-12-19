c9ea4915ab80402d9743a71659453d050f30388f
Chris Basham <chrisdbasham0317@gmail.com>
2019-12-19 12:14:23 -0700

    Add rest_framework to settings

--- intreped/settings.py
+++ intreped/settings.py
@@ -111,6 +111,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
