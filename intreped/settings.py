71a37539b136e10a6c7d78a3353207bc1ef03f47
Chris Basham <chrisdbasham0317@gmail.com>
2019-12-20 10:14:34 -0700

    Hardcode port for travis deployments

--- intreped/settings.py
+++ intreped/settings.py
@@ -76,11 +76,11 @@ if 'TRAVIS' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'travisci',
+            'NAME': 'intreped',
             'USER': 'postgres',
             'PASSWORD': '',
             'HOST': '',
-            'PORT': '',
+            'PORT': '5432',
         }
     }
 else:
